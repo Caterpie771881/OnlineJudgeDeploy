eccfc5c6b34588140a68ee4f515e3cdee16cd3c8
Caterpie <1915036350@qq.com>
2024-09-08 23:39:11 +0800

    add install.sh to change mirror address

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -n "please enter the mirror address: "
+
+read MIRROR
+
+if [ -n "$MIRROR" ] && [ "${MIRROR: -1}" != "/" ]; then
+    MIRROR+="/"
+fi
+
+echo $MIRROR
+
+cp `dirname $0`/docker-compose_.yml `dirname $0`/docker-compose.yml
+
+sed -i "s|<mirror>|$MIRROR|g" `dirname $0`/docker-compose.yml
+
+docker-compose up -d
+
